9ed8bc258d7ed6d977befd69432d16c39a6a2089
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-04-21 12:42:54 -0300

    customer

---
+++ MicroCRM.App/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using MicroCRM.App.Models.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroCRM.App.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new CustomerViewModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MicroCRM.App/Models/Customer/CustomerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroCRM.App.Models.Customer
+{
+    public class CustomerViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
