0f263cdaf6f2c59e06ac74ae5caaa37e26c44498
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-04-22 11:18:57 -0300

    lead

---
+++ MicroCRM.App/Controllers/LeadController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroCRM.App.Controllers
+{
+    public class LeadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View("./Form/index");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # my-micro-crm
+## Features
+### Leads
+- Criação de Form
+    - Multi etapas
+    - Campos dinâmicos
+### Customers
+- Cadastro
+### Stack
+- C#.NET MVC
+- MediatR
+- XUnit
