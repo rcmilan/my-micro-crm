decd4065c86ffa16ba03fedd157d013ab4e1a754
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-04-24 09:23:43 -0300

    CustomFormInputType

--- MicroCRM.App/Models/Form/CustomFormInputViewModel.cs
+++ MicroCRM.App/Models/Form/CustomFormInputViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace MicroCRM.App.Models.Form
+﻿using MicroCRM.Domain.Enums;
+
+namespace MicroCRM.App.Models.Form
 {
     public class CustomFormInputViewModel
     {
         public string Name { get; set; }
         public string Content { get; set; }
+        public CustomFormInputType InputType { get; set; }
     }
 }
